0868aa4a384db29d4a7c12d4dee07a5d38d874ea
gabrielmellace1 <gabrielmellace@gmail.com>
2024-04-15 18:23:14 -0300

    added r2

--- functions/helpers/image.ts
+++ functions/helpers/image.ts
@@ -4,32 +4,33 @@ export const downloadImage = async (updatedCID: string, r2Bucket: R2Bucket): Pro
   const timeoutId = setTimeout(() => controller.abort(), 60000); // 1 minute timeout
 
   try {
-      const response = await fetch(imageURL, { signal: controller.signal });
-      clearTimeout(timeoutId);
-      if (!response.ok) {
-          let errorCode;
-          switch (response.status) {
-              case 422: errorCode = 2; break;
-              case 404: errorCode = 3; break;
-              case 403: errorCode = 4; break;
-              case 500: errorCode = 5; break;
-              default: errorCode = 6; break;
-          }
-          return { success: false, errorCode };
-      }
+    const response = await fetch(imageURL, { signal: controller.signal });
+    clearTimeout(timeoutId);
+    if (!response.ok) {
+        let errorCode;
+        switch (response.status) {
+            case 422: errorCode = 2; break;
+            case 404: errorCode = 3; break;
+            case 403: errorCode = 4; break;
+            case 500: errorCode = 5; break;
+            default: errorCode = 6; break;
+        }
+        return { success: false, errorCode };
+    }
 
-      const imageBlob = await response.blob();
-      const contentType = response.headers.get('Content-Type');
-
-      await r2Bucket.put(updatedCID, imageBlob,
-        { httpMetadata: { contentType: contentType   } }
-        );
-      return { success: true };
-  } catch (error) {
-      clearTimeout(timeoutId);
-      if (error.name === 'AbortError') {
-          return { success: false, errorCode: 1 }; // Timeout error
+    const imageBlob = await response.blob();
+    const contentType = response.headers.get("Content-Type") || 'application/octet-stream'; // Default MIME type if not specified
+    await r2Bucket.put(updatedCID, imageBlob, {
+      httpMetadata: {
+        contentType: contentType
       }
-      return { success: false, errorCode: 6 }; // Other errors
+    });
+    return { success: true };
+  } catch ( error) {
+    clearTimeout(timeoutId);
+    if (error.name === 'AbortError') {
+        return { success: false, errorCode: 1 }; // Timeout error
+    }
+    return { success: false, errorCode: 6 }; // Other errors
   }
 };
