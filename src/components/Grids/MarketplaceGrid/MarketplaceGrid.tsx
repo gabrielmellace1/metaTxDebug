7ab3dd1cf19df7aac0126952efb03f6a7a199422
Jonathan <spasiuk@live.com>
2024-04-24 23:35:07 -0300

    feat(marketplace): add useraddress into marketplace grid

--- src/components/Grids/MarketplaceGrid/MarketplaceGrid.tsx
+++ src/components/Grids/MarketplaceGrid/MarketplaceGrid.tsx
@@ -1,10 +1,15 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { Coord, Layer, TileMap } from 'react-tile-map';
-import './MarketplaceGrid.css';
-import { COLOR_BY_TYPE, gridProps, switchColor } from '../../../helpers/GridHelper';
-import { AtlasTile } from '../../../types/atlasTypes';
-import Popup from './Popup/Popup';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Coord, Layer, TileMap } from "react-tile-map";
+import "./MarketplaceGrid.css";
+import {
+  COLOR_BY_TYPE,
+  gridProps,
+  switchColor,
+} from "../../../helpers/GridHelper";
+import { AtlasTile } from "../../../types/atlasTypes";
+import Popup from "./Popup/Popup";
+import { useAuth } from "../../../context/auth.context";
 
 let atlas: Record<string, AtlasTile> | null = null;
 let selected: Coord[] = [];
@@ -15,99 +20,20 @@ let userWallet = "0xea5fed1d0141f14de11249577921b08783d6a360";
 const getCoords = (x: number | string, y: number | string) => `${x},${y}`;
 
 async function loadTiles(setAtlasLoaded: (loaded: boolean) => void) {
-  const resp = await fetch('https://squares.town/api/graphSquares');
+  const resp = await fetch("https://squares.town/api/graphSquares");
   const json = await resp.json();
   atlas = json.data as Record<string, AtlasTile>;
   setAtlasLoaded(true);
 }
 
 function isSelected(x: number, y: number) {
-  return selected.some(coord => coord.x === x && coord.y === y);
+  return selected.some((coord) => coord.x === x && coord.y === y);
 }
 
-const atlasLayer: Layer = (x, y) => {
-  const id = getCoords(x, y);
-  if (atlas && id in atlas) {
-    const tile = atlas[id];
-    const color = COLOR_BY_TYPE[switchColor(tile.isOnState, tile.forSale, tile.owner, userWallet)];
-    return { color, top: !tile.top, left: !tile.left, topLeft: true };
-  } else {
-    return { color: (x + y) % 2 === 0 ? COLOR_BY_TYPE[8] : COLOR_BY_TYPE[9] };
-  }
-};
-
-const selectedStrokeLayer: Layer = (x, y) => isSelected(x, y) ? { color: '#ff0044', scale: 1.4 } : null;
-const selectedFillLayer: Layer = (x, y) => isSelected(x, y) ? { color: '#ff9990', scale: 1.2 } : null;
-
-async function handleClick(x: number, y: number) {
-  const id = getCoords(x, y);
-  const tile = atlas ? atlas[id] : null;
-
-  if (tile) {
-
-    if (!tile.forSale && tile.owner !== userWallet) {
-      return;
-    }
-      if (tile.isOnState) {
-        selected = [{ x, y }];
-          if (atlas) {
-            // Find other tiles with the same tokenId
-            Object.entries(atlas).forEach(([key, value]) => {
-                if (value.tokenId === tile.tokenId && key !== id) {
-                    const coords = key.split(',').map(Number);
-                    selected.push({ x: coords[0], y: coords[1] });
-                }
-            });
-        }
-          stateSelected = true;
-      } else {
-          if (stateSelected) {
-              selected = [];
-              stateSelected = false;
-          }
-
-          if (isSelected(x, y)) {
-              selected = selected.filter(coord => coord.x !== x || coord.y !== y);
-          } else {
-              selected.push({ x, y });
-          }
-      }
-      
-  }
-
-  // Determine actions based on the properties of the selected tiles
-  let canBuy = true, canSell = true, canCancel = true;
-  
-  for (const coord of selected) {
-    const tileId = getCoords(coord.x, coord.y);
-    const selTile = atlas?.[tileId]; // Correct usage of optional chaining
-    if (!selTile) continue;
-
-    if (selTile.owner !== userWallet || !selTile.forSale) {
-      canCancel = false; // Cannot cancel if any tile is not owned by the user or not for sale
-    }
-
-    if (selTile.owner !== userWallet || selTile.forSale ) {
-      canSell = false; // Cannot sell if any tile is not owned by the user
-    }
-
-    if (selTile.owner === userWallet || !selTile.forSale) {
-      canBuy = false; // Cannot buy if any tile is owned by the user or not for sale
-    }
-  }
-
-  // Output the status of actions
-  console.log('Actions enabled/disabled:', {
-    Buy: canBuy ? 'Enabled' : 'Disabled',
-    Sell: canSell ? 'Enabled' : 'Disabled',
-    Cancel: canCancel ? 'Enabled' : 'Disabled'
-  });
-
-  console.log('Selected Tiles:', selected);
-}
-
-
-
+const selectedStrokeLayer: Layer = (x, y) =>
+  isSelected(x, y) ? { color: "#ff0044", scale: 1.4 } : null;
+const selectedFillLayer: Layer = (x, y) =>
+  isSelected(x, y) ? { color: "#ff9990", scale: 1.2 } : null;
 
 const MarketplaceGrid: React.FC = () => {
   const [atlasLoaded, setAtlasLoaded] = useState(false);
@@ -115,6 +41,7 @@ const MarketplaceGrid: React.FC = () => {
   const [showPopup, setShowPopup] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
+  const { userAddress } = useAuth();
 
   useEffect(() => {
     loadTiles(setAtlasLoaded).catch(console.error);
@@ -123,10 +50,10 @@ const MarketplaceGrid: React.FC = () => {
       setMousePosition({ x: event.clientX, y: event.clientY });
     };
 
-    window.addEventListener('mousemove', updateMousePosition);
+    window.addEventListener("mousemove", updateMousePosition);
 
     return () => {
-      window.removeEventListener('mousemove', updateMousePosition);
+      window.removeEventListener("mousemove", updateMousePosition);
     };
   }, []);
 
@@ -137,7 +64,91 @@ const MarketplaceGrid: React.FC = () => {
     setShowPopup(tile !== null);
   };
 
- 
+  async function handleClick(x: number, y: number) {
+    const id = getCoords(x, y);
+    const tile = atlas ? atlas[id] : null;
+
+    if (tile) {
+      if (!tile.forSale && tile.owner !== userAddress) {
+        return;
+      }
+      if (tile.isOnState) {
+        selected = [{ x, y }];
+        if (atlas) {
+          // Find other tiles with the same tokenId
+          Object.entries(atlas).forEach(([key, value]) => {
+            if (value.tokenId === tile.tokenId && key !== id) {
+              const coords = key.split(",").map(Number);
+              selected.push({ x: coords[0], y: coords[1] });
+            }
+          });
+        }
+        stateSelected = true;
+      } else {
+        if (stateSelected) {
+          selected = [];
+          stateSelected = false;
+        }
+
+        if (isSelected(x, y)) {
+          selected = selected.filter((coord) => coord.x !== x || coord.y !== y);
+        } else {
+          selected.push({ x, y });
+        }
+      }
+    }
+
+    // Determine actions based on the properties of the selected tiles
+    let canBuy = true,
+      canSell = true,
+      canCancel = true;
+
+    for (const coord of selected) {
+      const tileId = getCoords(coord.x, coord.y);
+      const selTile = atlas?.[tileId]; // Correct usage of optional chaining
+      if (!selTile) continue;
+
+      if (selTile.owner !== userWallet || !selTile.forSale) {
+        canCancel = false; // Cannot cancel if any tile is not owned by the user or not for sale
+      }
+
+      if (selTile.owner !== userWallet || selTile.forSale) {
+        canSell = false; // Cannot sell if any tile is not owned by the user
+      }
+
+      if (selTile.owner === userWallet || !selTile.forSale) {
+        canBuy = false; // Cannot buy if any tile is owned by the user or not for sale
+      }
+    }
+
+    // Output the status of actions
+    console.log("Actions enabled/disabled:", {
+      Buy: canBuy ? "Enabled" : "Disabled",
+      Sell: canSell ? "Enabled" : "Disabled",
+      Cancel: canCancel ? "Enabled" : "Disabled",
+    });
+
+    console.log("Selected Tiles:", selected);
+  }
+
+  const atlasLayer: Layer = React.useCallback(
+    (x, y) => {
+      const id = getCoords(x, y);
+      if (atlas && id in atlas) {
+        const tile = atlas[id];
+        const color =
+          COLOR_BY_TYPE[
+            switchColor(tile.isOnState, tile.forSale, tile.owner, userAddress)
+          ];
+        return { color, top: !tile.top, left: !tile.left, topLeft: true };
+      } else {
+        return {
+          color: (x + y) % 2 === 0 ? COLOR_BY_TYPE[8] : COLOR_BY_TYPE[9],
+        };
+      }
+    },
+    [userAddress]
+  );
 
   return (
     <div className="grid-container">
@@ -149,13 +160,12 @@ const MarketplaceGrid: React.FC = () => {
             layers={[atlasLayer, selectedStrokeLayer, selectedFillLayer]}
             onClick={(x, y) => handleClick(x, y)}
             onHover={handleHover}
-            />
+          />
           {showPopup && hoveredTile && (
             <Popup
-              x={mousePosition.x-150}
-              y={mousePosition.y-350}
+              x={mousePosition.x - 150}
+              y={mousePosition.y - 350}
               tile={hoveredTile}
-              
             />
           )}
         </>
@@ -163,7 +173,7 @@ const MarketplaceGrid: React.FC = () => {
         <div>Loading atlas...</div>
       )}
     </div>
-    );
-  };
+  );
+};
 
 export default MarketplaceGrid;
